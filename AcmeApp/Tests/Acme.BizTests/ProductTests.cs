b2599cf7aef12aba3101f3c9a459134562a2968e
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-10-13 22:32:24 -0300

    Inicialização de Repositório

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Notebook";
+            currentProduct.ProductId = 1;
+            currentProduct.Description = "Notebook Acer Aspire 5";
+            var expected = "Hello Notebook (1): Notebook Acer Aspire 5";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
